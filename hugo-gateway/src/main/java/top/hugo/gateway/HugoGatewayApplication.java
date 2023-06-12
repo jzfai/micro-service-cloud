d6ffb8cb54d6326b7e5c74f672f2264249b4ecf9
jzfai <869653722@qq.com>
2023-06-12 14:45:56 +0800

    finish the first version

---
+++ hugo-gateway/src/main/java/top/hugo/gateway/HugoGatewayApplication.java
@@ -0,0 +1,16 @@
+package top.hugo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class HugoGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(HugoGatewayApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  网关启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
